148704d6cdf2cc410813a52c7219d9b5ce024b7b
PRANGYA PARAMITA <prangyachoudhury56@gmail.com>
2026-03-05 17:07:37 +0530

    pull

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HeroSection from '../../Component/HeroSection/HeroSection'
+
 
 const Home = () => {
   return (
     <div>
-      
+      <HeroSection/>
     </div>
   )
 }
