483be2c14ee023a94a458e67fa83b61eb142e81f
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-03-11 16:12:23 +0530

    Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,50 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 import Navbar from "./Component/Navbar/Navbar";
 import Footer from "./Component/Footer/Footer";
+import Loader from "./Component/Loader/Loader";
+
 import Home from "./Pages/Home/Home";
+
+function AppContent() {
+
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+
+    setLoading(true);
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // loader duration
+
+    return () => clearTimeout(timer);
+
+  }, [location]);
+
+  return (
+    <>
+      {loading && <Loader />}
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-
-      {/* Navbar */}
-      <Navbar />
-
-      {/* Page Content */}
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-      
-      
-       
-
-      </Routes>
-
-      {/* Footer */}
-      <Footer />
-
+      <AppContent />
     </BrowserRouter>
   );
 }
