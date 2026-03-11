0e2d2d6ec7a5dcfb74e9972cd35b999a14227532
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-03-11 14:36:07 +0530

    push hero and footer section

--- Frontend/src/Component/Footer/Footer.jsx
+++ Frontend/src/Component/Footer/Footer.jsx
@@ -1,102 +1,93 @@
 import React from "react";
 import "./Footer.css";
-import logo from "../../assets/Logo.webp";
+import logo from "../../assets/Tezz-Logo.webp";
 
 import {
   FaFacebookF,
   FaTwitter,
   FaLinkedinIn,
   FaInstagram,
-  FaSkype,
-  FaRss
 } from "react-icons/fa";
 
 import { IoSend } from "react-icons/io5";
 
 const Footer = () => {
   return (
-    <footer className="kgFooter-wrapper">
+    <footer className="kgFooter">
 
       <div className="kgFooter-container">
 
         {/* LEFT SECTION */}
-        <div className="kgFooter-aboutSection">
+        <div className="kgFooter-about">
+          <img src={logo} alt="logo" className="kgFooter-logo" />
 
-          <div className="kgFooter-logoBox">
-            <img src={logo} alt="logo" className="kgFooter-logoImg" />
-            
-          </div>
-
-          <p className="kgFooter-description">
-            Kindergarten school is a wonderland for Kids to learn,
-            play, and grow naturally and creatively.
+          <p>
+            Through a combination of lectures, readings and discussions,
+            students will gain solid foundation in education.
           </p>
 
-          <ul className="kgFooter-featureList">
-            <li>Advanced Educational Programs</li>
-            <li>Creativity & Thinking</li>
-            <li>Yoga & Swimming</li>
+          <div className="kgFooter-social">
+            <FaTwitter />
+            <FaFacebookF />
+            <FaLinkedinIn />
+            <FaInstagram />
+          </div>
+        </div>
+
+        {/* USEFUL LINKS */}
+        <div className="kgFooter-links">
+          <h3>Useful Link</h3>
+
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Event</li>
+            <li>Course</li>
+            <li>Blog Page</li>
+            <li>Teacher</li>
+            <li>Schedule</li>
+            <li>Contact</li>
           </ul>
-
         </div>
 
+        {/* CATEGORIES */}
+        <div className="kgFooter-category">
+          <h3>Categories</h3>
 
-        {/* CENTER SECTION */}
-        <div className="kgFooter-instagramSection">
-
-          <h3 className="kgFooter-sectionTitle yellowTitle">
-            Instagram
-          </h3>
-
-          <div className="kgFooter-titleUnderline yellowUnderline"></div>
-
+          <ul>
+            <li>Art Design</li>
+            <li>Graphic Design</li>
+            <li>Web Design</li>
+            <li>UX/UI Design</li>
+          </ul>
         </div>
 
+        {/* NEWSLETTER */}
+        <div className="kgFooter-newsletter">
+          <h3>Newsletter</h3>
 
-        {/* RIGHT SECTION */}
-        <div className="kgFooter-newsletterSection">
-
-          <h3 className="kgFooter-sectionTitle redTitle">
-            Newsletter Signup
-          </h3>
-
-          <div className="kgFooter-titleUnderline redUnderline"></div>
-
-          <p className="kgFooter-newsletterText">
-            Get Update Our News Everytime,
-            You Need Just Subscribe
+          <p>
+            Sign up for our newsletter and get 34% off your next course.
           </p>
 
           <div className="kgFooter-inputBox">
-
-            <input
-              type="email"
-              placeholder="Enter Your Email"
-              className="kgFooter-emailInput"
-            />
-
-            <button className="kgFooter-submitBtn">
+            <input type="email" placeholder="Enter Your Email*" />
+            <button>
               <IoSend />
             </button>
-
           </div>
 
-
-          {/* SOCIAL ICONS */}
-
-          <div className="kgFooter-socialIcons">
-
-            <FaFacebookF />
-            <FaTwitter />
-            <FaLinkedinIn />
-            <FaInstagram />
-            <FaSkype />
-            <FaRss />
-
+          <div className="kgFooter-checkbox">
+            <input type="checkbox" />
+            <span>I agree to the terms of use and privacy policy.</span>
           </div>
-
         </div>
 
+      </div>
+
+      {/* FOOTER BOTTOM */}
+      <div className="kgFooter-bottom">
+        Copyright © 2026 All Rights Reserved by Eduan
       </div>
 
     </footer>
